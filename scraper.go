f496b0a74fdf6d2a5850830f7712fc783167763e
joe <joeho7576@gmail.com>
2024-05-12 15:28:59 +0330

    rss aggregator saves posts in database

--- scraper.go
+++ scraper.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/joseph7576/rss-aggregator/internal/database"
 )
 
@@ -45,16 +48,44 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 
 	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
 	if err != nil {
-		log.Println("error marking feed as fetched: ", err)
+		log.Println("error marking feed as fetched:", err)
 	}
 
 	rssFeed, err := urlToFeed(feed.Url)
 	if err != nil {
-		log.Println("error fetching feed: ", err)
+		log.Println("error fetching feed:", err)
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post:", item.Title, "on feed", feed.Name)
+		desciption := sql.NullString{}
+
+		if item.Description != "" {
+			desciption.String = item.Description
+			desciption.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("couldn't parse date %v with err %v", item.PubDate, err)
+		}
+
+		_, err = db.CreatePost(context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: desciption,
+				PublishedAt: pubAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("failed to create post:", err)
+		}
 	}
 
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
